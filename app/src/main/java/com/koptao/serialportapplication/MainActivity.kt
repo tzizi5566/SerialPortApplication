0d23651a3d9d8c6a3f6e91f5e5caed6f5f5269b8
KopTao <tzizi5566@gmail.com>
2021-04-28 14:32:51 +0800

    更改代码仓库

--- app/src/main/java/com/koptao/serialportapplication/MainActivity.kt
+++ app/src/main/java/com/koptao/serialportapplication/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.koptao.serialportapplication
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
